920989e4bb3b556bb2b266a47d484f2ff9244bd9
Ayush Gupta <ayushgupta3@hotmail.com>
2019-02-01 22:46:31 -0500

    Working version of sorting into folders

--- organize.py
+++ organize.py
@@ -1,7 +1,39 @@
 import os
+DIRECTORIES = {
+    "HTML": [".html5", ".html", ".htm", ".xhtml"],
+    "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg"],
+    "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm"],
+    "DOCUMENTS": [".epub", ".docx", ".doc",".xps", ".dotx", ".docm", ".dox", ".xls", ".xlsx", ".ppt",".pptx"],
+    "ARCHIVES": [".iso", ".tar",".7z",".dmg", ".rar", ".zip"],
+    "AUDIO": [".mp3", ".msv",".raw", ".wav"],
+    "PLAINTEXT": [".txt", ".in", ".out"],
+    "PDF": [".pdf"],
+    "CS": [".py", ".java", ".c"],
+    "EXE": [".exe"],
+    "SHELL": [".sh"]
+}
+FILE_FORMATS = {file_format: directory for directory, file_formats in DIRECTORIES.items() for file_format in file_formats} 
 
-def OrganizeFiles():
+def organize_files():
+    create_directories()
     for file in os.listdir():
-        filename = os.fsdecode(file)
-        print(filename)
-OrganizeFiles()
+        if os.path.isdir(file):
+            continue
+        filename, fileextension = os.path.splitext(file)
+        if fileextension in FILE_FORMATS.keys():
+            if not file in os.listdir(FILE_FORMATS[fileextension.lower()]):
+                os.rename(file, os.path.join(FILE_FORMATS[fileextension.lower()], file))
+    delete_empty_directories()
+        
+def delete_empty_directories():
+    for key in DIRECTORIES.keys():
+        if not os.listdir(key):
+            os.rmdir(key)
+
+def create_directories():
+    for key in DIRECTORIES.keys():
+        if not os.path.exists(key):
+            os.mkdir(key)
+
+if __name__ == "__main__":
+    organize_files()
